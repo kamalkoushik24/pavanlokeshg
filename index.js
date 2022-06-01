f1c60a6dd653c066fb629218688fb16a38cc57aa
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-01 17:40:10 +0530

    Working smtp commit

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-const { getMaxListeners } = require('process');
-define(function send(){
- 
-    
-
-
-    var nodemailer = require('nodemailer');
-
-    const form = document.getElementById("contact-form");
-    const email = document.getElementById("Email").value; // making a function send to get all values in the form and display it in the console for testing
-
-    const name = document.getElementById("Name").value;
-    const subject = document.getElementById("subject").value;
-    const body = document.getElementById("projdesc").value;
-    form.submit();
-    form.reset();
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'gpavanlokeshnoreply@gmail.com',
-            pass: 'pavan@123'
-        }
-    });
-    var mailOptions = {
-        from: name,
-        to: 'pavanloakeshnaiduguruju@gmail.com',
-        subject: subject,
-        text: body
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-})
+function sendEmail(){
+  document = index.html;
+  message = "Your Query has been Submitted and will be reviewed.";
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "pavanlokeshnoreplygmail.com",
+    Password : "pavan@123",
+    To : 'pavanlokeshnaiduguruju@gmail.com',
+    From : document.getElementById("Email").value,
+    Subject : document.getElementById("Subject").value,
+    Body : document.getElementById("projdesc").value,
+}).then(
+  
+  message => alert(message)
+);
+}
