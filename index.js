2290ac4959173be0ebfb3817d4baeb059f0791c0
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-01 18:24:11 +0530

    Working SMTP commit 4

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function sendEmail(){
   message = "Your Query has been Submitted and will be reviewed.";
   Email.send({
     Host : "smtp.elasticemail.com",
-    Username : "pavanlokeshnoreplygmail.com",
-    Password : "pavan@123",
+    Username : "gpavanlokeshnoreplygmail.com",
+    Password : "9F7345FE6EB7CFE92181828AA639196E0A63",
     To : 'pavanlokeshnaiduguruju@gmail.com',
     From : document.getElementById("Email").value,
     Subject : document.getElementById("Subject").value,
