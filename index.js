3b2366eb4a6d1e96128a77b7d5eac2ee118c4a0a
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-02 16:33:06 +0530

    Final working commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function sendEmail(){
   
   message = "Your Query has been Submitted and will be reviewed.";
   Email.send({
-    SecureToken :" 3d955485-a74d-4f2f-b386-748dee39f4ca",
+    SecureToken :"3d955485-a74d-4f2f-b386-748dee39f4ca",
     To : 'pavanlokeshnaiduguruju@gmail.com',
     From : document.getElementById("Email").value,
     Subject : document.getElementById("Subject").value,
