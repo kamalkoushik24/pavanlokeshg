fb69f1002d872b5cb214018384d82594bb5f635d
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-02 14:46:04 +0530

    Mail fix 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sendEmail(){
   
   message = "Your Query has been Submitted and will be reviewed.";
   Email.send({
-    SecureToken :"91bf5d7c-eb16-4431-8376-dda928109339",
+    SecureToken :" 3d955485-a74d-4f2f-b386-748dee39f4ca",
     To : 'pavanlokeshnaiduguruju@gmail.com',
     From : document.getElementById("Email").value,
     Subject : document.getElementById("Subject").value,
