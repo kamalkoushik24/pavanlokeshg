6cce8f5790e65761cc61f0e92176586e5dc9937c
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-02 15:06:17 +0530

    Mail fix 5

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const { default: tinymce } = require("tinymce");
 
 function sendEmail(){
+
+  tinymce.init({
+    selector: '#projdesc',
+    height: 400,
+    width: 500,
+    plugins: [
+      'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+      'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+      'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+    ],
+    toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+      'alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+  });
   
   message = "Your Query has been Submitted and will be reviewed.";
   Email.send({
