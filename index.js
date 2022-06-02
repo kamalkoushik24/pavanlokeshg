5cc6151368c3c4db8a9271a86eb52e65053add58
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-02 17:26:40 +0530

    Commiting

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function sendEmail(){
   var body = tinymce.activeEditor.getContent({format:'txt'})
   
   message = "Your Query has been Submitted and will be reviewed.";
+ 
   Email.send({
     SecureToken :"974c6701-f3d3-49f8-ae97-be55779cd4d0",
     To : 'kamalkoushik234@gmail.com',
@@ -19,8 +20,6 @@ function sendEmail(){
   message => alert(message)
   
 );
-return false,
-location.reload();
-document.getElementById("contact-form").reset();
+
 
 }
