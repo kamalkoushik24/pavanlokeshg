0616dd43f2c615661c41f8a58e8b29b09e0337ec
kamalkoushik24 <kamalkoushik24@gmail.com>
2022-06-02 09:04:51 +0530

    SMTP working commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sendEmail(){
   
   message = "Your Query has been Submitted and will be reviewed.";
   Email.send({
-    secureToken :"91bf5d7c-eb16-4431-8376-dda928109339",
+    SecureToken :"91bf5d7c-eb16-4431-8376-dda928109339",
     To : 'pavanlokeshnaiduguruju@gmail.com',
     From : document.getElementById("Email").value,
     Subject : document.getElementById("Subject").value,
